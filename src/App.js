ab590c9b73396db23ca814e3175fba4bacd30252
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-22 09:44:32 +0530

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Chat from "./components/Chat/Chat";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Chat App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Chat />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/404" element={<h1>Choose the correct path</h1>} />
+          <Route path="*" element={<Navigate replace to="/404" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
