c808e74f10b9a684dcde93c6a2c0cf5555c6ab9b
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-22 19:36:12 +0530

    Authenticate user using axios

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import loginImage from "../../assets/images/login.svg";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./Auth.scss";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitForm = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://127.0.0.1:3000/login", { email, password })
+      .then((res) => {
+        console.log("res", res);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+    console.log({ email, password });
+  };
   return (
     <div id="auth-container">
       <div id="auth-card">
@@ -12,12 +27,24 @@ const Login = () => {
           </div>
           <div id="form-section">
             <h1>Welcome back</h1>
-            <form>
+            <form onSubmit={submitForm}>
               <div className="input-field mb-1">
-                <input placeholder="Email" />
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  required="required"
+                  type="text"
+                  placeholder="Email"
+                />
               </div>
               <div className="input-field mb-2">
-                <input placeholder="Password" />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required="required"
+                  type="password"
+                  placeholder="Password"
+                />
               </div>
               <button>LOGIN</button>
             </form>
