6f3ac682ed648ba758cfad883b411394e9e392c3
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-22 10:18:21 +0530

    Create Login component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,33 @@
 import React from "react";
+import loginImage from "../../assets/images/login.svg";
 
 const Login = () => {
-  return <h1>Login Screen</h1>;
+  return (
+    <div id="auth-container">
+      <div id="auth-card">
+        <div>
+          <div id="image-section">
+            <img src={loginImage} alt="Login">
+              {" "}
+            </img>
+          </div>
+          <div id="form-section">
+            <h1>Welcome back</h1>
+            <form>
+              <div className="input-field">
+                <input placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <input placeholder="Password" />
+              </div>
+              <button>lOGIN</button>
+            </form>
+            <p>Don't have an account? Register</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
