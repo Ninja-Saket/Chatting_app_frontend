48df639911713e1feb539b3a3e85fdc8c8d446c6
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-22 10:46:10 +0530

    Style Login component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,23 +1,21 @@
 import React from "react";
 import loginImage from "../../assets/images/login.svg";
-
+import "./Auth.scss";
 const Login = () => {
   return (
     <div id="auth-container">
       <div id="auth-card">
-        <div>
+        <div className="card-shadow">
           <div id="image-section">
-            <img src={loginImage} alt="Login">
-              {" "}
-            </img>
+            <img src={loginImage} alt="Login"></img>
           </div>
           <div id="form-section">
             <h1>Welcome back</h1>
             <form>
-              <div className="input-field">
+              <div className="input-field mb-1">
                 <input placeholder="Email" />
               </div>
-              <div className="input-field">
+              <div className="input-field mb-2">
                 <input placeholder="Password" />
               </div>
               <button>lOGIN</button>
