df70bbb07536f21fd7d7b54550ddf67d8ef2a64d
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-24 14:46:35 +0530

    Create chat navbar

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
+import Navbar from "./components/Navbar/Navbar";
+import "./Chat.scss";
 const Chat = () => {
-  const user = useSelector((state) => state.authReducer.user);
   return (
-    <div>
-      <h1>Chat Screen</h1>
-      <p>Welcome, {user.firstName}</p>
+    <div id="chat-container">
+      <Navbar />
+      <div id="char-wrap">Data</div>
     </div>
   );
 };
