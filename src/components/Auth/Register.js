d75b7f84faa019f25260b1f4efdca6e6cb61cfe8
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-24 09:16:47 +0530

    Register a new user

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import registerImage from "../../assets/images/register.svg";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Auth.scss";
-
+import { useDispatch } from "react-redux";
+import { register } from "../../store/actions/auth";
 const Register = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("male");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitForm = (event) => {
+    event.preventDefault();
+
+    dispatch(register({ firstName, lastName, email, gender, password })).then(
+      navigate("/")
+    );
+    // AuthService.login({ email, password }).then((res) => console.log(res));
+    // axios
+    //   .post("http://127.0.0.1:3000/login", { email, password })
+    //   .then((res) => {
+    //     console.log("res", res);
+    //   })
+    //   .catch((err) => {
+    //     console.log("err", err);
+    //   });
+    // console.log({ email, password });
+  };
   return (
     <div id="auth-container">
       <div id="auth-card">
@@ -13,24 +40,52 @@ const Register = () => {
           </div>
           <div id="form-section">
             <h1>Create an account</h1>
-            <form>
+            <form onSubmit={submitForm}>
               <div className="input-field mb-1">
-                <input placeholder="First name" />
+                <input
+                  onChange={(e) => setFirstName(e.target.value)}
+                  value={firstName}
+                  required="required"
+                  type="text"
+                  placeholder="First name"
+                />
               </div>
               <div className="input-field mb-1">
-                <input placeholder="Last name" />
+                <input
+                  onChange={(e) => setLastName(e.target.value)}
+                  value={lastName}
+                  required="required"
+                  type="text"
+                  placeholder="Last name"
+                />
               </div>
               <div className="input-field mb-1">
-                <input placeholder="Email" />
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  required="required"
+                  type="text"
+                  placeholder="Email"
+                />
               </div>
               <div className="input-field mb-1">
-                <select>
+                <select
+                  onChange={(e) => setGender(e.target.value)}
+                  value={gender}
+                  required="required"
+                >
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                 </select>
               </div>
               <div className="input-field mb-2">
-                <input placeholder="Password" />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required="required"
+                  type="password"
+                  placeholder="Password"
+                />
               </div>
               <button>REGISTER</button>
             </form>
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,5 +1,4 @@
-import { LOGIN } from "../actions/auth";
-
+import { LOGIN, REGISTER } from "../actions/auth";
 const initialState = {
   user: {},
   token: "",
@@ -10,6 +9,13 @@ const authReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
     case LOGIN:
+      return {
+        ...state,
+        user: payload,
+        token: payload.token,
+        isLoggedIn: true,
+      };
+    case REGISTER:
       return {
         ...state,
         user: payload,
