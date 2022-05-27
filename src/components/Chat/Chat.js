da98525775b2628e5596416ff25786eed3025c87
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-27 17:45:23 +0530

    Fetch chats and save in store

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Navbar from "./components/Navbar/Navbar";
+import { fetchChats } from "../../store/actions/chat";
 import "./Chat.scss";
 const Chat = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.authReducer.user);
+  useEffect(() => {
+    dispatch(fetchChats())
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, [dispatch]);
   return (
     <div id="chat-container">
       <Navbar />
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./auth";
+import chatReducer from "./auth";
 
 export default combineReducers({
   authReducer,
+  chatReducer,
 });
