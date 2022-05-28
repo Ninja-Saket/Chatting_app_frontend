19bcd1433990adff5b04283d4153653370612e14
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-28 11:04:27 +0530

    Fix chatReducer import

--- src/store/reducers/chat.js
+++ src/store/reducers/chat.js
@@ -9,11 +9,12 @@ const chatReducer = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case FETCH_CHATS:
+    case FETCH_CHATS: {
       return {
         ...state,
         chats: payload,
       };
+    }
     default: {
       return state;
     }
