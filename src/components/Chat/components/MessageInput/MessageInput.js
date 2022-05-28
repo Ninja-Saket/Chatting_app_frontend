4994961c73b1be15f6a9955d042f56de70e83017
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-28 20:14:35 +0530

    Create message input component

--- src/components/Chat/components/MessageInput/MessageInput.js
+++ src/components/Chat/components/MessageInput/MessageInput.js
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "./MessageInput.scss";
 
-const MessageInput = () => {
-  return <h1>MessageInput</h1>;
+const MessageInput = ({ chat }) => {
+  const user = useSelector((state) => state.authReducer.user);
+
+  const [message, setMessage] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleMessage = (e) => {
+    const value = e.target.value;
+    setMessage(value);
+
+    // Notify other users that this user is typing something
+  };
+
+  const handleKeyDown = (e, imageUpload) => {
+    if (e.key === "Enter") {
+      sendMessage(imageUpload);
+    }
+  };
+
+  const sendMessage = (imageUpload) => {
+    if (message.length < 1 && !imageUpload) return;
+    const msg = {
+      type: imageUpload ? "image" : "text",
+      fromUserId: user.id,
+      toUserId: chat.Users.map((user) => user.id),
+      chatId: chat.id,
+      message: imageUpload ? image : message,
+    };
+
+    setMessage("");
+    setImage("");
+
+    // Send message with sockets
+  };
+  return (
+    <div id="input-container">
+      <div id="message-input">
+        <input
+          type="text"
+          placeholder="Message..."
+          onChange={(e) => handleMessage(e)}
+          onKeyDown={(e) => handleKeyDown(e, false)}
+        />
+        <FontAwesomeIcon icon={["far", "smile"]} className="fa-icon" />
+      </div>
+    </div>
+  );
 };
 
 export default MessageInput;
