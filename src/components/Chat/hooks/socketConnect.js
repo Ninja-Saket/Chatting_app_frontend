49ce56872e00214460d386c8715e1e7f36bae423
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-29 16:43:58 +0530

    Fix user online/offline status update bug
    Making sure that when we run status update reducers we have our chats in store

--- src/components/Chat/hooks/socketConnect.js
+++ src/components/Chat/hooks/socketConnect.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import {
+  fetchChats,
   onlineFriends,
   onlineFriend,
   offlineFriend,
@@ -8,23 +9,29 @@ import socketIOClient from "socket.io-client";
 
 const useSocket = (user, dispatch) => {
   useEffect(() => {
-    const socket = socketIOClient.connect("http://127.0.0.1:3000");
-    socket.emit("join", user);
-    socket.on("typing", (user) => {
-      console.log("Event", user);
-    });
-    socket.on("friends", (friends) => {
-      console.log("Friends", friends);
-      dispatch(onlineFriends(friends));
-    });
-    socket.on("online", (user) => {
-      console.log("Online", user);
-      dispatch(onlineFriend(user));
-    });
-    socket.on("offline", (user) => {
-      console.log("Offline", user);
-      dispatch(offlineFriend(user));
-    });
+    dispatch(fetchChats())
+      .then((res) => {
+        const socket = socketIOClient.connect("http://127.0.0.1:3000");
+        socket.emit("join", user);
+        socket.on("typing", (user) => {
+          console.log("Event", user);
+        });
+        socket.on("friends", (friends) => {
+          console.log("Friends", friends);
+          dispatch(onlineFriends(friends));
+        });
+        socket.on("online", (user) => {
+          console.log("Online", user);
+          dispatch(onlineFriend(user));
+        });
+        socket.on("offline", (user) => {
+          console.log("Offline", user);
+          dispatch(offlineFriend(user));
+        });
+
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   }, [dispatch]);
 };
 
