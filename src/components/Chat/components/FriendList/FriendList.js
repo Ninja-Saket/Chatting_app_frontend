ac33b0e34787383bf23536fb3e293bb0d45660db
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-31 16:18:02 +0530

    create searchUsers inside chatService

--- src/components/Chat/components/FriendList/FriendList.js
+++ src/components/Chat/components/FriendList/FriendList.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { setCurrentChat } from "../../../../store/actions/chat";
 import Friend from "../Friend/Friend";
 import Modal from "../../../Modal/Modal";
+import ChatService from "../../../../services/chatService";
 
 import "./FriendList.scss";
 
@@ -11,24 +12,31 @@ const FriendList = () => {
   const [suggestions, setSuggestions] = useState([]);
   const dispatch = useDispatch();
   const chats = useSelector((state) => state.chatReducer.chats);
+  const socket = useSelector((state) => state.chatReducer.socket);
 
   const openChat = (chat) => {
     dispatch(setCurrentChat(chat));
   };
 
   const searchFriends = (e) => {
-    // chat service
+    ChatService.searchUsers(e.target.value).then((res) => setSuggestions(res));
   };
 
   const addNewFriend = (id) => {
     // dispatch
+    ChatService.createChat(id)
+      .then((chats) => {
+        socket.emit("add-friend", chats);
+        setShowFriendsModal(false);
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <div id="friends" className="shadow-light">
       <div id="title">
         <h3 className="m-0">Friends</h3>
-        <button>ADD</button>
+        <button onClick={() => setShowFriendsModal(true)}>ADD</button>
       </div>
       <hr />
       <div id="friends-box">
@@ -43,7 +51,7 @@ const FriendList = () => {
         )}
       </div>
       {showFriendsModal && (
-        <Modal>
+        <Modal closeModalHandler={() => setShowFriendsModal(false)}>
           <Fragment key="header" />
           <h3 className="m-0">Create new chat</h3>
           <Fragment />
@@ -58,7 +66,7 @@ const FriendList = () => {
             <div id="suggestions">
               {suggestions.map((user) => {
                 return (
-                  <div className="suggestion">
+                  <div key={user.id} className="suggestion">
                     <p className="m-0">
                       {user.firstName} {user.lastName}
                     </p>
